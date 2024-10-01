129b549cffd935b9a550452ee0f7218e38c56a6e
omgSRT <thangckdt@gmail.com>
2024-10-01 12:55:29 +0700

    fix: remove dockerfile

---
+++ API/Controllers/PressController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class PressController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
