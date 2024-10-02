b93eabd39bcf4ba071f26be79a964333170de742
omgSRT <thangckdt@gmail.com>
2024-10-02 19:51:43 +0700

    feat: update db book data

--- BusinessObject/ApplicationDbContext.cs
+++ BusinessObject/ApplicationDbContext.cs
@@ -24,7 +24,7 @@ namespace BusinessObject
         {
             IConfiguration config = new ConfigurationBuilder()
                 .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", true, true)
+                .AddJsonFile("appsettingsjson", true, true)
                 .Build();
             var strConn = config["ConnectionStrings:DB"];
 
@@ -67,6 +67,29 @@ namespace BusinessObject
                 new Press { PressId = 8, PressName = "Lao Dong Publishing House", CategoryId = 2},
                 new Press { PressId = 9, PressName = "Science And Technology Publishing House", CategoryId = 3},
                 new Press { PressId = 10, PressName = "Culture And Arts Publishing House", CategoryId = 3}
+            );
+
+            modelBuilder.Entity<Book>().HasData(
+                new Book { BookId = 1, IBSN = "9786042073143", Title = "Doraemon", Author = "Fujiko F. Fujio", Price = 14376, AddressId = 1, PressId = 1 },
+                new Book { BookId = 2, IBSN = "9786041073007", Title = "The Little Prince", Author = "Antoine de Saint-Exupéry", Price = 25200, AddressId = 2, PressId = 2 },
+                new Book { BookId = 3, IBSN = "9786043097919", Title = "Ho Chi Minh: A Life", Author = "William Duiker", Price = 30600, AddressId = 3, PressId = 3 },
+                new Book { BookId = 4, IBSN = "9786041156545", Title = "The Tale of Kieu", Author = "Nguyen Du", Price = 18000, AddressId = 4, PressId = 4 },
+                new Book { BookId = 5, IBSN = "9786042129877", Title = "Forbes Magazine", Author = "Forbes Media", Price = 16776, AddressId = 5, PressId = 5 },
+                new Book { BookId = 6, IBSN = "9786042028981", Title = "Textbook of Chemistry", Author = "Nguyen Quang Hieu", Price = 36000, AddressId = 6, PressId = 6 },
+                new Book { BookId = 7, IBSN = "9786042130425", Title = "Women's World Magazine", Author = "Various", Price = 11976, AddressId = 7, PressId = 7 },
+                new Book { BookId = 8, IBSN = "9786042032517", Title = "Labor and Employment Law", Author = "Nguyen Van Thanh", Price = 22200, AddressId = 8, PressId = 8 },
+                new Book { BookId = 9, IBSN = "9786043049989", Title = "Principles of Technology", Author = "Le Van Minh", Price = 45576, AddressId = 9, PressId = 9 },
+                new Book { BookId = 10, IBSN = "9786045068940", Title = "Vietnamese Art Through the Ages", Author = "Le Huy Toan", Price = 49200, AddressId = 10, PressId = 10 },
+                new Book { BookId = 11, IBSN = "9786041072545", Title = "Harry Potter and the Philosopher's Stone", Author = "J.K. Rowling", Price = 21576, AddressId = 1, PressId = 2 },
+                new Book { BookId = 12, IBSN = "9786042067812", Title = "A Brief History of Vietnam", Author = "Truong Buu Lam", Price = 33000, AddressId = 2, PressId = 3 },
+                new Book { BookId = 13, IBSN = "9786043152176", Title = "Cooking with Love", Author = "Le Thi Kim", Price = 14376, AddressId = 3, PressId = 4 },
+                new Book { BookId = 14, IBSN = "9786041092182", Title = "Business News Magazine", Author = "Various Authors", Price = 18000, AddressId = 4, PressId = 5 },
+                new Book { BookId = 15, IBSN = "9786042076541", Title = "Advanced Mathematics", Author = "Pham Van Dinh", Price = 39600, AddressId = 5, PressId = 6 },
+                new Book { BookId = 16, IBSN = "9786042151430", Title = "Women's Weekly Magazine", Author = "Various", Price = 95760, AddressId = 6, PressId = 7 },
+                new Book { BookId = 17, IBSN = "9786043087103", Title = "Employment Law Handbook", Author = "Nguyen Thi Lan", Price = 26400, AddressId = 7, PressId = 8 },
+                new Book { BookId = 18, IBSN = "9786044087217", Title = "The Science of Everything", Author = "Le Duc Thinh", Price = 54600, AddressId = 8, PressId = 9 },
+                new Book { BookId = 19, IBSN = "9786045089128", Title = "Vietnamese Folk Art", Author = "Tran Quang Huy", Price = 61200, AddressId = 9, PressId = 10 },
+                new Book { BookId = 20, IBSN = "9786042075652", Title = "Dragon Ball", Author = "Akira Toriyama", Price = 15600, AddressId = 10, PressId = 1 }
             );
         }
     }
---
+++ BusinessObject/Migrations/20241002125015_Update Book Data.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BusinessObject.Migrations
+{
+    public partial class UpdateBookData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Books",
+                columns: new[] { "BookId", "AddressId", "Author", "IBSN", "PressId", "Price", "Title" },
+                values: new object[,]
+                {
+                    { 1, 1, "Fujiko F. Fujio", "9786042073143", 1, 14376m, "Doraemon" },
+                    { 2, 2, "Antoine de Saint-Exupéry", "9786041073007", 2, 25200m, "The Little Prince" },
+                    { 3, 3, "William Duiker", "9786043097919", 3, 30600m, "Ho Chi Minh: A Life" },
+                    { 4, 4, "Nguyen Du", "9786041156545", 4, 18000m, "The Tale of Kieu" },
+                    { 5, 5, "Forbes Media", "9786042129877", 5, 16776m, "Forbes Magazine" },
+                    { 6, 6, "Nguyen Quang Hieu", "9786042028981", 6, 36000m, "Textbook of Chemistry" },
+                    { 7, 7, "Various", "9786042130425", 7, 11976m, "Women's World Magazine" },
+                    { 8, 8, "Nguyen Van Thanh", "9786042032517", 8, 22200m, "Labor and Employment Law" },
+                    { 9, 9, "Le Van Minh", "9786043049989", 9, 45576m, "Principles of Technology" },
+                    { 10, 10, "Le Huy Toan", "9786045068940", 10, 49200m, "Vietnamese Art Through the Ages" },
+                    { 11, 1, "J.K. Rowling", "9786041072545", 2, 21576m, "Harry Potter and the Philosopher's Stone" },
+                    { 12, 2, "Truong Buu Lam", "9786042067812", 3, 33000m, "A Brief History of Vietnam" },
+                    { 13, 3, "Le Thi Kim", "9786043152176", 4, 14376m, "Cooking with Love" },
+                    { 14, 4, "Various Authors", "9786041092182", 5, 18000m, "Business News Magazine" },
+                    { 15, 5, "Pham Van Dinh", "9786042076541", 6, 39600m, "Advanced Mathematics" },
+                    { 16, 6, "Various", "9786042151430", 7, 95760m, "Women's Weekly Magazine" },
+                    { 17, 7, "Nguyen Thi Lan", "9786043087103", 8, 26400m, "Employment Law Handbook" },
+                    { 18, 8, "Le Duc Thinh", "9786044087217", 9, 54600m, "The Science of Everything" },
+                    { 19, 9, "Tran Quang Huy", "9786045089128", 10, 61200m, "Vietnamese Folk Art" },
+                    { 20, 10, "Akira Toriyama", "9786042075652", 1, 15600m, "Dragon Ball" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "BookId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "BookId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "BookId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "BookId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "BookId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "BookId",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "BookId",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "BookId",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "BookId",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "BookId",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "BookId",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "BookId",
+                keyValue: 12);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "BookId",
+                keyValue: 13);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "BookId",
+                keyValue: 14);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "BookId",
+                keyValue: 15);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "BookId",
+                keyValue: 16);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "BookId",
+                keyValue: 17);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "BookId",
+                keyValue: 18);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "BookId",
+                keyValue: 19);
+
+            migrationBuilder.DeleteData(
+                table: "Books",
+                keyColumn: "BookId",
+                keyValue: 20);
+        }
+    }
+}
